bce6912a0637ad3998238ae80921299f6ad78006
Sagun Panthi <sagunpanthi@Saguns-MacBook-Air.local>
2022-11-09 15:16:02 +1100

    commit

---
+++ netflix/client/src/App.js
@@ -0,0 +1,15 @@
+import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
+import "./app.scss"
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+
+
+function App() {
+  return (
+  
+   <Login />
+  );
+}
+
+export default App;
