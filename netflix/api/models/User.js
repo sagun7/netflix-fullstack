2a141c543aeabac079da5ceb51a7c670ae218950
Sagun Panthi <sagunpanthi@Saguns-MacBook-Air.local>
2022-11-10 15:16:12 +1100

    api

--- netflix/api/models/User.js
+++ netflix/api/models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true,  },
-    email: { type: String, required: true, },
+    username: { type: String, required: true, unique: true  },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     profilePic: { type: String, defaut: "" },
     isAdmin: { type: Boolean, default: false },
